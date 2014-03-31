2ede9ba24f3b8c1fd5fe35680d0d678c7cc3c371
William Dodson <william.dodson@gmail.com>
2014-03-31 15:08:58 -0400

    Updated contact form with new fields and added additional styles and information to contact page.

--- a/j/site.js
+++ a/j/site.js
@@ -1,3 +1,23 @@
+// Nice responsive iframe script from Aaron Gustafson: https://gist.github.com/aarongustafson/1313517
+function adjustIframes() {
+  $('iframe').each(function() {
+    var
+    $this       = $(this),
+    proportion  = $this.data( 'proportion' ),
+    w           = $this.attr('width'),
+    actual_w    = $this.width();
+    
+    if (!proportion) {
+	    proportion = $this.attr('height') / w;
+	    $this.data('proportion', proportion);
+    }
+  
+    if (actual_w != w) {
+			$this.css('height', Math.round( actual_w * proportion ) + 'px');
+    }
+  });
+}
+
 // Web Font Loader
 WebFont.load({
   google: {
@@ -10,3 +30,6 @@ var nav = responsiveNav('.nav-collapse', {
   insert: 'after',
   label: '<i class="icon-menu"></i> Menu'
 });
+
+// onload/resize
+$(window).on('resize load',adjustIframes);
